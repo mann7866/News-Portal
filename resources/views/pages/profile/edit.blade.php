d78d43152335cf093765e193468b600db9fd4eab
IqbalFirmans <iqbalclan74@gmail.com>
2024-11-01 15:52:41 +0700

    update edit profile

--- resources/views/pages/profile/edit.blade.php
+++ resources/views/pages/profile/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    
+
     {{-- <x-slot name="header">
         <h2 class="font-weight-bold text-dark">
             {{ __('Profile Information') }}
@@ -16,7 +16,7 @@
                     <p class="small text-muted mb-0">Update your account's profile information and email address.</p>
                 </div> --}}
                 <div class="card-body">
-                    @include('profile.partials.update-profile-information-form')
+                    @include('pages.profile.partials.update-profile-information-form')
                 </div>
             </div>
 
@@ -27,14 +27,14 @@
                     <p class="small text-muted mb-0">Ensure your account is using a long, random password to stay secure.</p>
                 </div>
                 <div class="card-body">
-                    @include('profile.partials.update-password-form')
+                    @include('pages.profile.partials.update-password-form')
                 </div>
             </div>
 
             <!-- Delete Account Section -->
             <div class="card mb-4">
                 <div class="card-body">
-                    @include('profile.partials.delete-user-form')
+                    @include('pages.profile.partials.delete-user-form')
                 </div>
             </div>
         </div>
