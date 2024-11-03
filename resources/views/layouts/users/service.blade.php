6e829160691ba53bef1fcd1c606eecc5c0d7913f
Mardev <rozakadm@gmail.com>
2024-11-03 22:28:15 +0700

    Copy

---
+++ resources/views/layouts/users/service.blade.php
@@ -0,0 +1,66 @@
+<section class="section db p120">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="tagline-message text-center">
+                    <h3>Howdy, we are Sekolah, we have brought together the best quality services, offers, projects for you!</h3>
+                </div>
+            </div><!-- end col -->
+        </div><!-- end row -->
+    </div><!-- end container -->
+</section><!-- end section -->
+
+<section class="section gb nopadtop">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="box m30">
+                    <i class="flaticon-computer-tool-for-education"></i>
+                    <h4>Learning system</h4>
+                    <p>All sections required for online training are included to Sekolah.</p>
+                    <a href="#" class="readmore">Read more</a>
+                </div>
+            </div><!-- end col -->
+
+            <div class="col-md-6">
+                <div class="box m30">
+                    <i class="flaticon-monitor-tablet-and-smartohone"></i>
+                    <h4>Works all mobile devices</h4>
+                    <p>The most important feature of this template is that it is compatible with all mobile devices. Your customers can also visit your site easily from tablets and phones.</p>
+                    <a href="#" class="readmore">Read more</a>
+                </div>
+            </div><!-- end col -->
+
+            <div class="col-md-3">
+                <div class="box m30">
+                    <i class="flaticon-download-business-statistics-symbol-of-a-graphic"></i>
+                    <h4>User Dashboard</h4>
+                    <p>We designed the design of all the sub-pages needed for the users.</p>
+                    <a href="#" class="readmore">Read more</a>
+                </div>
+            </div><!-- end col -->
+        </div><!-- end row -->
+
+        <hr class="invis">
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="box">
+                    <i class="flaticon-html5"></i> <i class="flaticon-css-3"></i>
+                    <h4>Compatible HTML5 & CSS3</h4>
+                    <p>HTML5 is a markup language used for structuring and presenting content on the World Wide Web. It is the fifth and current version of the HTML standard.</p>
+                    <a href="#" class="readmore">Read more</a>
+                </div>
+            </div><!-- end col -->
+
+            <div class="col-md-6">
+                <div class="box">
+                    <i class="flaticon-html-coding"></i>
+                    <h4>Bootstrap Framework</h4>
+                    <p>Bootstrap is a technique of loading a program into a computer by means of a few initial instructions which enable the introduction of the rest of the program from an input device.</p>
+                    <a href="#" class="readmore">Read more</a>
+                </div>
+            </div><!-- end col -->
+        </div><!-- end row -->
+    </div><!-- end container -->
+</section>
--- resources/views/pages/profile/edit.blade.php
+++ resources/views/pages/profile/edit.blade.php
@@ -1,20 +1,9 @@
 @extends('layouts.app')
 @section('content')
-
-    {{-- <x-slot name="header">
-        <h2 class="font-weight-bold text-dark">
-            {{ __('Profile Information') }}
-        </h2>
-    </x-slot> --}}
-
     <div class="py-5">
         <div class="container">
             <!-- Profile Information Section -->
             <div class="card mb-4">
-                {{-- <div class="card-header bg-white">
-                    <h3 class="h5 mb-0">{{ __('Profile Information') }}</h3>
-                    <p class="small text-muted mb-0">Update your account's profile information and email address.</p>
-                </div> --}}
                 <div class="card-body">
                     @include('pages.profile.partials.update-profile-information-form')
                 </div>
