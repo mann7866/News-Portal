e1b5bdb368e0c0e8b9ce0bb74f72fe2ff4a27246
mann7866 <akunkudewe861@gmail.com>
2024-11-03 11:23:00 +0700

    menambahkan form pada create berita

--- resources/views/pages/super-admin/news/create.blade.php
+++ resources/views/pages/super-admin/news/create.blade.php
@@ -26,92 +26,96 @@
         </div>
     </div>
 
-    <div class="card rounded-2 overflow-hidden">
-        <div class="position-relative">
-            <input type="file" accept="image/*" id="imageInput" style="display: none;" onchange="previewImage(event)">
-            <label for="imageInput">
-                <img id="imagePreview" src="{{ asset('admin-assets/images/blog/blog-img5.jpg') }}"
-                    class="card-img-top rounded-0" alt="Preview" style="width: 100%; height: auto; cursor: pointer; object-fit: cover;">
-            </label>
-        </div>
+    <form action="">
+        <div class="card rounded-2 overflow-hidden">
+            <div class="position-relative">
+                <input type="file" accept="image/*" id="imageInput" style="display: none;"
+                    onchange="previewImage(event)">
+                <label for="imageInput">
+                    <img id="imagePreview" src="{{ asset('admin-assets/images/blog/blog-img5.jpg') }}"
+                        class="card-img-top rounded-0" alt="Preview"
+                        style="width: 100%; height: auto; cursor: pointer; object-fit: cover;">
+                </label>
+            </div>
 
 
-        <div class="row">
+            <div class="row">
 
-            <div class="col-12">
-                <div class="card">
+                <div class="col-12">
+                    <div class="card">
 
-                    <div class="border-bottom title-part-padding">
-                        <div class="floating-labels mt-4">
-                            <div class="form-group mb-5 focused">
-                                <input type="text" class="form-control" id="input1" style="font-weight: bold;" />
-                                <span class="bar"></span>
-                                <label class="" for="input1" style="font-weight: bold;">
-                                    <h4>
-                                        Judul Berita
-                                    </h4>
-                                </label>
+                        <div class="border-bottom title-part-padding">
+                            <div class="floating-labels mt-4">
+                                <div class="form-group mb-5 focused">
+                                    <input type="text" class="form-control" id="input1" style="font-weight: bold;" />
+                                    <span class="bar"></span>
+                                    <label class="" for="input1" style="font-weight: bold;">
+                                        <h4>
+                                            Judul Berita
+                                        </h4>
+                                    </label>
+                                </div>
                             </div>
-                        </div>
 
-                        <h5 for="input1">Deskripsi Berita</h5>
-                    </div>
-                    <div class="card-body">
-                        <div class="summernote">
-                            <h4></h4>
+                            <h5 for="input1">Deskripsi Berita</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="summernote">
+                                <h4></h4>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12">
-                <div class="card">
-                    <div class="border-bottom title-part-padding">
-                        <div class="">
-                            <label>Kategori Berita <span class="text-danger">*</span></label>
-                            <select class="select2 form-control" multiple="multiple" style="height: 36px; width: 100%">
-                                <optgroup label="Alaskan/Hawaiian Time Zone">
-                                    <option value="AK">Alaska</option>
-                                    <option value="HI">Hawaii</option>
-                                </optgroup>
-                                <optgroup label="Pacific Time Zone">
-                                    <option value="CA">California</option>
-                                    <option value="NV">Nevada</option>
-                                    <option value="OR">Oregon</option>
-                                    <option value="WA">Washington</option>
-                                </optgroup>
-                            </select>
+                <div class="col-12">
+                    <div class="card">
+                        <div class="border-bottom title-part-padding">
+                            <div class="">
+                                <label>Kategori Berita <span class="text-danger">*</span></label>
+                                <select class="select2 form-control" multiple="multiple" style="height: 36px; width: 100%">
+                                    <optgroup label="Alaskan/Hawaiian Time Zone">
+                                        <option value="AK">Alaska</option>
+                                        <option value="HI">Hawaii</option>
+                                    </optgroup>
+                                    <optgroup label="Pacific Time Zone">
+                                        <option value="CA">California</option>
+                                        <option value="NV">Nevada</option>
+                                        <option value="OR">Oregon</option>
+                                        <option value="WA">Washington</option>
+                                    </optgroup>
+                                </select>
+                            </div>
+
+                            <div class="d-flex flex-column mt-3">
+                                <div class="mb-3 form-group">
+                                    <label>Start Date
+                                        <span class="text-danger">*</span></label>
+                                    <div class="controls">
+                                        <input type="date" name="start_date" class="form-control" required
+                                            data-validation-required-message="This field is required" />
+                                    </div>
+                                    <div class="form-control-feedback">
+                                        <small><code>Opsional</code></small>
+                                    </div>
+                                </div>
+                                <div class="mb-3 form-group">
+                                    <label>End Date
+                                        <span class="text-danger">*</span></label>
+                                    <div class="controls">
+                                        <input type="date" name="end_date" class="form-control" required
+                                            data-validation-required-message="This field is required" />
+                                    </div>
+                                    <div class="form-control-feedback">
+                                        <small><code>Opsional</code></small>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
-                        <div class="d-flex flex-column mt-3">
-                            <div class="mb-3 form-group">
-                                <label>Start Date
-                                    <span class="text-danger">*</span></label>
-                                <div class="controls">
-                                    <input type="date" name="start_date" class="form-control" required
-                                        data-validation-required-message="This field is required" />
-                                </div>
-                                <div class="form-control-feedback">
-                                    <small><code>Opsional</code></small>
-                                </div>
-                            </div>
-                            <div class="mb-3 form-group">
-                                <label>End Date
-                                    <span class="text-danger">*</span></label>
-                                <div class="controls">
-                                    <input type="date" name="end_date" class="form-control" required
-                                        data-validation-required-message="This field is required" />
-                                </div>
-                                <div class="form-control-feedback">
-                                    <small><code>Opsional</code></small>
-                                </div>
-                            </div>
-                        </div>
                     </div>
-
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 @endsection
 <script>
     function previewImage(event) {
