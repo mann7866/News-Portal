b18a367a0884059869a65469301f9b63906f0b99
Mardev <rozakadm@gmail.com>
2024-11-04 12:34:44 +0700

    update

--- resources/views/pages/super-admin/account/index.blade.php
+++ resources/views/pages/super-admin/account/index.blade.php
@@ -111,6 +111,34 @@
                                         </p>
                                     </td>
                                 </tr>
+                                <tr>
+                                    <td class="border-0 ps-0">
+                                        <div class="d-flex align-items-center">
+                                            <div class="me-2 pe-1">
+                                                <img src="{{ asset('admin-assets/images/profile/user-5.jpg') }}"
+                                                    class="rounded-circle" width="40" height="40" alt="" />
+                                            </div>
+                                            <div>
+                                                <h6 class="fw-semibold mb-1">{{ Auth::user()->name }}</h6>
+                                                <p class="fs-2 mb-0 text-muted">{{ Auth::user()->email }}</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="border-0">
+                                        <span class="badge fw-semibold py-1 w-85 bg-light-success text-success">Online</span>
+                                    </td>
+                                    <td class="border-0">
+                                        <p class="fs-3 text-dark mb-0">
+                                            <button class="btn btn-danger btn-sm">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                    fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                                                </svg>
+                                            </button>
+                                        </p>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
