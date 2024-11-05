12ec7dbd624449ed313b2f5b36279546b70a1484
Fahris12344 <farismhd255@gmail.com>
2024-11-05 09:35:34 +0700

    tittle

--- resources/views/layouts/users/topbar.blade.php
+++ resources/views/layouts/users/topbar.blade.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <!-- Site Meta -->
-    <title>Sekolah Demonic</title>
+    <title>News Portal</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
