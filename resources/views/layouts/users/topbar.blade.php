6bf44c6a45e743bdc4f7b9de5a6c44b74c6bf6c4
Fahris12344 <farismhd255@gmail.com>
2024-11-05 09:45:44 +0700

    tobar

--- resources/views/layouts/users/topbar.blade.php
+++ resources/views/layouts/users/topbar.blade.php
@@ -70,38 +70,6 @@
             </div>
         </div>
         <header class="header">
-            <div class="topbar clearfix">
-                <div class="container">
-                    <div class="row-fluid">
-                        <div class="col-md-6 col-sm-6 text-left">
-                            <p>
-                                <strong><i class="fa fa-phone"></i></strong> +62 815 4874 3796 &nbsp;&nbsp;
-                                <strong><i class="fa fa-envelope"></i></strong> <a
-                                    href="mailto:#">tzyarchon@gmail.com</a>
-                            </p>
-                        </div><!-- end left -->
-                        <div class="col-md-6 col-sm-6 hidden-xs text-right">
-                            <div class="social">
-                                <a class="facebook"
-                                    href="https://www.facebook.com/gaming/play?store_visit_source=games_bookmark"
-                                    data-tooltip="tooltip" data-placement="bottom" title="Facebook"><i
-                                        class="fa fa-facebook"></i></a>
-                                <a class="twitter" href="https://x.com/bni" data-tooltip="tooltip"
-                                    data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
-                                <a class="google"
-                                    href="https://edu.google.com/intl/ALL_id/communications/?utm_source=8a_googleEDU&utm_medium=cpc&utm_campaign=global-EDU-operational-cs-edu-fa-gp-cpm-paid-media-audience-expansion&utm_content=21145112370&utm_term=google%20for%20education&gad_source=1&gclid=CjwKCAjwjsi4BhB5EiwAFAL0YHdbhtDBy44wsKgywnjBqI88ZS0p8Fvona2wykGOgOteo0cptQeBHhoC3NQQAvD_BwE&gclsrc=aw.ds"
-                                    data-tooltip="tooltip" data-placement="bottom" title="Google Plus"><i
-                                        class="fa fa-google-plus"></i></a>
-                                <a class="linkedin" href="https://www.linkedin.com/learning" data-tooltip="tooltip"
-                                    data-placement="bottom" title="Linkedin"><i class="fa fa-linkedin"></i></a>
-                                <a class="pinterest" href="https://id.pinterest.com/search/pins/?q=sekolah&rs=typed"
-                                    data-tooltip="tooltip" data-placement="bottom" title="Pinterest"><i
-                                        class="fa fa-pinterest"></i></a>
-                            </div><!-- end social -->
-                        </div><!-- end left -->
-                    </div><!-- end row -->
-                </div><!-- end container -->
-            </div><!-- end topbar -->
             <!-- END # MODAL LOGIN -->
 
             <div class="container">
@@ -115,9 +83,10 @@
                             <span class="icon-bar"></span>
                         </button>
                         <div class="logo-normal">
-                            <a class="navbar-brand" href="index.html"><img src="{{asset('users-assets/images/logo.png')}}"
-                                    alt=""></a>
-                        </div>
+                            <a class="navbar-brand" href="index.html">
+                                <img src="{{ asset('users-assets/images/logo-smk-putih.png') }}" alt="" style="width: 250px; height: auto;">
+                            </a>
+                        </div>                        
                     </div>
 
                     <div id="navbar" class="navbar-collapse collapse">
@@ -201,11 +170,11 @@
         <div class="copyrights">
             <div class="container">
                 <div class="clearfix">
-                    <div class="pull-left">
-                        <div class="cop-logo">
-                            <a href="#"><img src="{{asset('users-assets/images/logo.png')}}" alt=""></a>
-                        </div>
-                    </div>
+                    <div class="logo-normal">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="{{ asset('users-assets/images/logo-smk-putih.png') }}" alt="" style="width: 250px; height: auto;">
+                        </a>
+                    </div> 
 
                     <div class="pull-right">
                         <div class="footer-links">
