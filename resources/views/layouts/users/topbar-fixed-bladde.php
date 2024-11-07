5d0c0302fe97901cfce14af128a8cef9f9cc28bb
Mardev <rozakadm@gmail.com>
2024-11-07 11:39:13 +0700

    main

--- resources/views/layouts/users/topbar-fixed-bladde.php
+++ resources/views/layouts/users/topbar-fixed-bladde.php
@@ -1,32 +1,22 @@
 <!doctype html>
-<!--[if IE 9]> <html class="no-js ie9 fixed-layout" lang="en"> <![endif]-->
-<!--[if gt IE 9]><!-->
-<html class="no-js " lang="en"> <!--<![endif]-->
-
+<html class="no-js" lang="en">
 <head>
-
-    <!-- Basic -->
+    <!-- Basic Meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <!-- Mobile Meta -->
-    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <!-- Site Meta -->
     <title>News Portal</title>
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
     <!-- Site Icons -->
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico" type="image/x-icon') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon" />
     <link rel="apple-touch-icon" href="{{ asset('assets/images/apple-touch-icon.png') }}">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,400i,700,700i" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
 
     <!-- Custom & Default Styles -->
     <link rel="stylesheet" href="{{ asset('users-assets/css/bootstrap.min.css') }}">
@@ -35,61 +25,36 @@
     <link rel="stylesheet" href="{{ asset('users-assets/css/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('users-assets/style.css') }}">
 
-    <!--[if lt IE 9]>
-  <script src="js/vendor/html5shiv.min.js"></script>
-  <script src="js/vendor/respond.min.js"></script>
- <![endif]-->
+    <style>
+        /* Navbar transparan di bagian atas */
+        #navbar {
+            background-color: transparent; /* Background transparan */
+            transition: background-color 0.3s ease; /* Transisi halus */
+        }
 
+        /* Navbar berubah menjadi putih saat di-scroll */
+        #navbar.scrolled {
+            background-color: white; /* Background putih */
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Shadow untuk efek floating */
+        }
+    </style>
 </head>
-<style>
-    .sticky-top {
-    transition: backdrop-filter 0.3s ease, background-color 0.3s ease;
-}
 
-.sticky-top.scrolled {
-    backdrop-filter: blur(10px);
-    background-color: rgba(255, 255, 255, 0.8); 
-    /* Warna dengan transparansi */
-}
-
-</style>
 <body>
     <!-- LOADER -->
     <div id="preloader">
         <img class="preloader" src="{{ asset('users-assets/images/loader.gif') }}" alt="">
-    </div><!-- end loader -->
+    </div>
     <!-- END LOADER -->
 
     <div id="wrapper">
-        <!-- BEGIN # MODAL LOGIN -->
-        {{-- Top bar --}}
-        <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-hidden="true"
-            style="display: none;">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <!-- Begin # DIV Form -->
-                    <div id="div-forms">
-                        <form id="login-form">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span class="flaticon-add" aria-hidden="true"></span>
-                            </button>
-                            <div class="modal-body">
-                                <input class="form-control" type="text" placeholder="What you are looking for?"
-                                    required>
-                            </div>
-                        </form><!-- End # Login Form -->
-                    </div><!-- End # DIV Form -->
-                </div>
-            </div>
-        </div>
+        <!-- Navbar -->
         <header class="header">
-            <!-- END # MODAL LOGIN -->
-
             <div class="container">
-                <nav class="navbar navbar-default yamm sticky-top" id="navbar">
+                <nav id="navbar" class="navbar navbar-default yamm">
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                            data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            data-target="#navbar-content" aria-expanded="false">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
@@ -103,11 +68,9 @@
                         </div>
                     </div>
 
-                    <div id="navbar" class="navbar-collapse collapse">
+                    <div id="navbar-content" class="navbar-collapse collapse">
                         <ul class="nav navbar-nav navbar-right">
                             <li><a href="{{route('dashboard')}}">Home</a></li>
-                         
-                           
                             <li><a href="page-contact.html">Contact</a></li>
                             <li><a href="{{route('login')}}">Login</a></li>
                             <li><a href="{{route('register')}}">Register</a></li>
@@ -116,20 +79,34 @@
                                     <i class="fa fa-search"></i>
                                 </a>
                             </li>
-                            
                         </ul>
                     </div>
-                </nav><!-- end navbar -->
-            </div><!-- end container -->
+                </nav>
+            </div>
         </header>
 
+        <!-- Content Section -->
         @yield('content')
 
+        <!-- Footer -->
         @include('layouts.users.footer')
+    </div>
 
-
-        {{-- script Src --}}
-        <script>
+    <!-- Script untuk mengubah background navbar saat di-scroll -->
+    <script>
+        // Event listener untuk scroll
+        window.addEventListener('scroll', function() {
+            const navbar = document.getElementById('navbar');
+            
+            // Menambahkan kelas 'scrolled' saat halaman di-scroll lebih dari 50px
+            if (window.scrollY > 50) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        });
+    </script>
+ <script>
            document.addEventListener('keydown', function(event) {
         if (event.ctrlKey && event.key === 'h') {
             event.preventDefault();
@@ -137,15 +114,5 @@
         }
     });
         </script>
-       <script>
-        // Mendengarkan peristiwa scroll pada window
-        window.addEventListener('scroll', function() {
-            const navbar = document.getElementById('navbar');
-            if (window.scrollY > 50) { // Jika scroll lebih dari 50px
-                navbar.classList.add('scrolled');
-            } else {
-                navbar.classList.remove('scrolled');
-            }
-        });
-    </script>
-    
+</body>
+</html>
