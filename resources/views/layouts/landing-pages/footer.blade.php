8566145aa0d10cad1a70b4f84a8eed8bd6b64377
mann7866 <akunkudewe861@gmail.com>
2024-11-10 21:14:14 +0700

    pembenahan

--- resources/views/layouts/landing-pages/footer.blade.php
+++ resources/views/layouts/landing-pages/footer.blade.php
@@ -221,6 +221,6 @@ if (currentURL == "https://themeforest.net/") {
 }
 });
 </script>
-
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 </body>
