e0c3d5cf050f24d7c535dda123a3a35d83e29ac6
IqbalFirmans <iqbalclan74@gmail.com>
2024-11-11 11:48:47 +0700

    update halaman news

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,26 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt(value: 'password')
-        ]);
-
-        Category::create([
-            'name' => 'Event'
-        ]);
-        Category::create([
-            'name' => 'School'
-        ]);
-        Category::create([
-            'name' => 'Coding'
-        ]);
-        Category::create([
-            'name' => 'Competition'
-        ]);
-        Category::create([
-            'name' => 'Contest'
+        $this->call([
+            CategorySeeder::class,
+            UserSeeder::class
         ]);
     }
 }
