5aa38cd2fb4ac6f44dd05402130041f7d80cf99c
Fahris12344 <farismhd255@gmail.com>
2024-11-19 14:24:40 +0700

    tampilan search

--- resources/views/pages/landing-page/news/partials/side-content.blade.php
+++ resources/views/pages/landing-page/news/partials/side-content.blade.php
@@ -2,15 +2,16 @@
     <aside>
         <div class="sidebar-item search">
             <div class="sidebar-info">
-                <form action="{{ route('news') }}" method="GET" class="mb-4">
+                <form action="{{ route('news') }}" method="GET" class="mb-4 d-flex align-items-center">
                     <input type="text" name="search" class="form-control" placeholder="Cari berita atau kategori..."
-                           value="{{ request('search') }}">
-                    <button type="submit" class="btn btn-primary mt-2">
+                        value="{{ request('search') }}">
+                    <button type="submit" class="btn btn-primary">
                         <i class="fas fa-search"></i>
                     </button>
-                </form>                
+                </form>
             </div>
         </div>
+
         <div class="sidebar-item recent-post">
             <div class="title">
                 <h4>Berita Populer</h4>
